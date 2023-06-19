caf3a3fd5f7b6a2654bef0def4a0833e649239a7
Erich Barnstedt <erichb@microsoft.com>
2023-06-19 14:56:23 +0200

    Initial checkin.

---
+++ Model.cs
@@ -0,0 +1,33 @@
+﻿
+namespace PressureRelief
+{
+    using System;
+
+    class RequestModel
+    {
+        public string? Command { get; set; }
+
+        public Guid CorrelationId { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public string? Endpoint { get; set; }
+
+        public string? MethodNodeId { get; set; }
+
+        public string? ParentNodeId { get; set; }
+
+        public string[]? Arguments { get; set; }
+    }
+
+    class ResponseModel
+    {
+        public Guid CorrelationId { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+        public bool Success { get; set; }
+
+        public string? Status { get; set; }
+    }
+}
